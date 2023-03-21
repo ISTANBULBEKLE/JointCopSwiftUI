ad37af65a85817fa8a06cd056e7a53d381908ae7
ISTANBULBEKLE <ekipkalir@gmail.com>
2023-03-21 10:01:00 +0000

    Initial Commit

---
+++ JointCopSwiftUI/JointCopSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  JointCopSwiftUIApp.swift
+//  JointCopSwiftUI
+//
+//  Created by ekip.kalir on 21/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct JointCopSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
